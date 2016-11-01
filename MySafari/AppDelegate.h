3b7cd7b113432430c3a2942963a0c88c3959eade
RobertLeeL <RobertLee@robertleedemacbook-pro.local>
2016-11-01 19:25:38 +0800

    Initial Commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by 李龙跃 on 16/11/1.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
